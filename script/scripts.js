85076056695b86defab0d16a831e870f03dbfa67
Luisa Nievas <luisa.nievas94@gmail.com>
2024-01-15 16:37:57 -0300

    Começando a trabalhar com Web Workers e Paralelismo

--- script/scripts.js
+++ script/scripts.js
@@ -1,26 +1,21 @@
+import imprimeCotacao from "./imprimeCotacao.js";
 const graficoDolar = document.getElementById('graficoDolar');
 
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: [],
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        label: 'Dólar',
+        data: [],
         borderWidth: 1
       }]
     }    
     
 });
 
-  async function conectaAPI() {
-    const conecta = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL');
-    const conectaTraduzido =  await conecta.json();
-
-    console.log(conectaTraduzido);
-}
-
 setInterval(()=> conectaAPI(), 5000);
+
 
 function geraHorario(){
     let data = new Date();
@@ -29,4 +24,15 @@ function geraHorario(){
     return horario; 
 }
 
-geraHorario();
+function adicionarDados(grafico,legenda,dados){
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach((dataset) => {
+    dataset.data.push(dados);
+     
+  })
+
+  grafico.update();
+}
+
+let workerDolar = new Worker('./script/workers/workerDolar.js');
+workerDolar.postMessage('usd');
